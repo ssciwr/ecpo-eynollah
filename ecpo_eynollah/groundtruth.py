858285aabbf45ed05f95669b8a86ed11f2ea9a45
Dominic Kempf <dominic.kempf@iwr.uni-heidelberg.de>
2025-10-15 10:24:42 +0200

    Use IIIF metadata instead of actual image for size detection

--- ecpo_eynollah/groundtruth.py
+++ ecpo_eynollah/groundtruth.py
@@ -1,13 +1,12 @@
 """Massaging of existing ground truth into desired formats """
 
 import click
+import functools
 import io
 import json
-import os
 import pathlib
 import requests
 import svgelements
-import tifffile
 
 from urllib.parse import unquote
 
@@ -22,17 +21,6 @@ def _recursive_unquote(url):
     return unquote(unquoted)
 
 
-def _find_filename_with_inconsistent_naming_scheme(filename):
-    _variants = [lambda fn: fn, lambda fn: fn.replace("+", "to")]
-
-    for var in _variants:
-        candidate = var(filename)
-        if os.path.exists(candidate):
-            return candidate
-
-    raise FileNotFoundError(f"The file {filename} was nowhere to be found")
-
-
 def _is_data_json(filename):
     """Some rules to exclude JSON files in globbing that occurred during development"""
     if ".ipynb_checkpoints" in str(filename):
@@ -43,7 +31,26 @@ def _is_data_json(filename):
     return True
 
 
-def annotation_to_labelstudio(annotation, image_root):
+@functools.lru_cache
+def iiif_metadata(url):
+    """Access IIIF metadata for the given URL. Also corrects the URL from
+    old server in the ecpo-data repository to the currently running one.
+    """
+    # Create a live URL for use with LabelStudio without duplicating the
+    # data on our LabelStudio VM.
+    old_iiif = _recursive_unquote(url)
+    iiif = old_iiif.replace(
+        "https://kjc-sv002.kjc.uni-heidelberg.de:8080/fcgi-bin/iipsrv.fcgi?IIIF=imageStorage/ecpo_new/",
+        "https://ecpo.cats.uni-heidelberg.de/fcgi-bin/iipsrv.fcgi?IIIF=/ecpo/images/",
+    )
+    iiif = iiif.replace("+", "to")
+
+    # Check the validity of the IIIF URL by accessing the metadata file
+    info_url = iiif.replace("/full/full/0/default.jpg", "/info.json")
+    return requests.get(info_url, timeout=5).json()
+
+
+def annotation_to_labelstudio(annotation):
     """Translates a single annotation from ecpo-annotate to LabelStudio"""
 
     def _body_target_combination(body, target):
@@ -81,28 +88,9 @@ def annotation_to_labelstudio(annotation, image_root):
         # Find image data for this annotation (wild)
         #
 
-        image_url = target["source"]
-
-        # We have double-encoded URLs
-        image_url = _recursive_unquote(image_url)
-
-        # We need to remove all the IIIF stuff - we want to use local data,
-        # otherwise we are creating a lot of network traffic to get data that
-        # we already have readily available
-        filename = image_url.replace(
-            "https://kjc-sv002.kjc.uni-heidelberg.de:8080/fcgi-bin/iipsrv.fcgi?IIIF=imageStorage/ecpo_new/",
-            "",
-        ).replace("/full/full/0/default.jpg", "")
-        filename = _find_filename_with_inconsistent_naming_scheme(
-            str(image_root / filename)
-        )
-
-        # Extract the image size
-        with tifffile.TiffFile(filename) as tif:
-            # Each level of the pyramid is a "page"
-            original_sized = tif.pages[0]
-            width = original_sized.imagewidth
-            height = original_sized.imagelength
+        metadata = iiif_metadata(target["source"])
+        width = metadata["width"]
+        height = metadata["height"]
 
         #
         # Convert the SVG selector to global coordinates
@@ -142,18 +130,18 @@ def annotation_to_labelstudio(annotation, image_root):
             yield from _body_target_combination(body, target)
 
 
-def image_annotations_to_labelstudio(data, image_root):
+def image_annotations_to_labelstudio(data):
     """Translates all annotations on a single image to LabelStudio"""
 
     annotations = []
     for annot in data["items"]:
-        for polygon in annotation_to_labelstudio(annot, image_root):
+        for polygon in annotation_to_labelstudio(annot):
             annotations.append(polygon)
 
     return annotations
 
 
-def ecpo_data_to_labelstudio(input, output, image_root):
+def ecpo_data_to_labelstudio(input, output):
     # Find all valid JSON files in the input
     json_files = list(filter(_is_data_json, pathlib.Path(input).rglob("*.json")))
 
@@ -166,23 +154,13 @@ def ecpo_data_to_labelstudio(input, output, image_root):
             data = json.load(f)
 
         # Get all annotations from this JSON file
-        annotations = image_annotations_to_labelstudio(data, pathlib.Path(image_root))
+        annotations = image_annotations_to_labelstudio(data)
 
-        # Create a live URL for use with LabelStudio without duplicating the
-        # data on our LabelStudio VM.
-        old_iiif = data["items"][0]["target"][0]["source"]
-        old_iiif = _recursive_unquote(old_iiif)
-        iiif = old_iiif.replace(
-            "https://kjc-sv002.kjc.uni-heidelberg.de:8080/fcgi-bin/iipsrv.fcgi?IIIF=imageStorage/ecpo_new/",
-            "https://ecpo.cats.uni-heidelberg.de/fcgi-bin/iipsrv.fcgi?IIIF=/ecpo/images/",
+        # Create the required IIIF URL
+        iiif = (
+            iiif_metadata(data["items"][0]["target"][0]["source"])["id"]
+            + "/full/full/0/default.jpg"
         )
-        iiif = iiif.replace("+", "to")
-
-        # Check the validity of the IIIF URL by accessing the metadata file
-        info_url = iiif.replace("/full/full/0/default.jpg", "/info.json")
-        r = requests.head(info_url, timeout=5)
-        if r.status_code != 200:
-            raise ValueError(f"This IIIF URL seems to be incorrect: {iiif}")
 
         tasks.append(
             {"id": i, "data": {"image": iiif}, "annotations": [{"result": annotations}]}
@@ -217,26 +195,13 @@ def ecpo_data_to_labelstudio(input, output, image_root):
     default="labelstudio.json",
     help="Where to write the LabelStudio output. Must be a directory.",
 )
-@click.option(
-    "--image-root",
-    type=click.Path(
-        writable=False,
-        file_okay=False,
-        dir_okay=True,
-        exists=True,
-        resolve_path=True,
-        path_type=pathlib.Path,
-    ),
-    help="The root folder of the (local) image directory e.g. your SDS@HD mount",
-)
 @click.command
-def cli(input, output, image_root):
-    ecpo_data_to_labelstudio(input, output, image_root)
+def cli(input, output):
+    ecpo_data_to_labelstudio(input, output)
 
 
 if __name__ == "__main__":
     ecpo_data_to_labelstudio(
         pathlib.Path("../ecpo-data/data/annotator_data/JB-visualGT/"),
         pathlib.Path("labelstudio_data"),
-        pathlib.Path("/home/dkempf/heibox/ECPO_data/images_with_groundtruth/ptif"),
     )
