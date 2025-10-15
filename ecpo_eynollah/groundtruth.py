102e1662311193831334ebabe14d102f4289bc5f
Dominic Kempf <dominic.kempf@iwr.uni-heidelberg.de>
2025-10-15 17:01:12 +0200

    Refactor to deal with additional SVG elements

--- ecpo_eynollah/groundtruth.py
+++ ecpo_eynollah/groundtruth.py
@@ -100,28 +100,34 @@ def annotation_to_labelstudio(annotation):
         if len(svg) > 1:
             raise ValueError("Expected only one element in SVG string")
         svg = svg[0]
-        if not isinstance(svg, svgelements.Polygon):
-            print(f"Omitting SVG element of type {type(svg)}")
+
+        # Treat polygonal annotations
+        if isinstance(svg, svgelements.Polygon):
+            # Transform into simple data structure for JSON dump. LabelStudio uses
+            # a percentage of the original size as data representation.
+            points = [[p.x / width * 100.0, p.y / height * 100.0] for p in svg]
+
+            yield {
+                "original_width": width,
+                "original_height": height,
+                "image_rotation": 0,
+                "value": {
+                    "points": points,
+                    "closed": True,
+                    "polygonlabels": [label],
+                },
+                "id": target["id"],
+                "from_name": "label",
+                "to_name": "image",
+                "type": "polygonlabels",
+            }
             return
 
-        # Transform into simple data structure for JSON dump. LabelStudio uses
-        # a percentage of the original size as data representation.
-        points = [[p.x / width * 100.0, p.y / height * 100.0] for p in svg]
+        if isinstance(svg, svgelements.Circle):
+            print("Circle found")
+            return
 
-        yield {
-            "original_width": width,
-            "original_height": height,
-            "image_rotation": 0,
-            "value": {
-                "points": points,
-                "closed": True,
-                "polygonlabels": [label],
-            },
-            "id": target["id"],
-            "from_name": "label",
-            "to_name": "image",
-            "type": "polygonlabels",
-        }
+        print(f"Omitting SVG element of type {type(svg)}")
 
     # Traverse the combinations of body/target
     for body in annotation["body"]:
@@ -207,5 +213,5 @@ def cli(input, output):
 if __name__ == "__main__":
     ecpo_data_to_labelstudio(
         pathlib.Path("../ecpo-data/data/annotator_data/JB-visualGT/"),
-        pathlib.Path("labelstudio_data"),
+        pathlib.Path("labelstudio_data.json"),
     )
